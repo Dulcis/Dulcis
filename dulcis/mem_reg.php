5cef0018bb2db3c7147e73778faa210a2e8a4a17
Nitaro16 <nitta.yuu@gmail.com>
2014-05-18 18:10:41 +0900

    かなり多くの修正をしました。 細かなところに手が届いてない感が否めませんが だいたいこんなかんじでいいんでしょうか？

---
+++ dulcis/mem_reg.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"　"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<link href="shop.css" rel="stylesheet" type="text/css" />
+
+<title>Dulcis</title>
+</head>
+<body>
+
+<?php include 'ref/header.php';?>
+
+
+
+
+<?php include 'ref/reg.php';?>
+
+
+<?php include 'ref/footer.php';?>
+
+</body>
+</html>
---
+++ dulcis/ref/cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"　"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<link href="shop.css" rel="stylesheet" type="text/css" />
+
+<title>Dulcis</title>
+</head>
+<body>
+<div id="favorite">
+<p>○○さんのカートの中身です</p>
+<a href="#" ><img src="#" width="160" height="160" alt="#" />ハイチュウ</a>
+<a href="#" ><img src="#" width="160" height="160" alt="#" />haityu</a>
+<a href="#" ><img src="#" width="160" height="160" alt="#" />highchoo</a>
+</div>
+</body>
+</html>
