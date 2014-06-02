9b4d872d60c252ba75336e47dcd7a8b8beb6e757
Nitaro16 <nitta.yuu@gmail.com>
2014-06-02 23:26:40 +0900

    jsファイル3つ追加　CSS追加 会員登録画面にdivなどを追加したものをアップ 家で動作確認できなかったので、学校でやります

---
+++ dulcis/app/res/js/hidden.js
@@ -0,0 +1,14 @@
+function hyoji1(num)
+{
+  if (num == 0)
+  {
+    document.getElementById("disp").style.display="block";
+  }
+  else
+  {
+    document.getElementById("disp").style.display="none";
+  }
+}
+
+/*ネタバレ防止などによく使われるクリックすると文字が出て来るアイツです
+ただ不具合が多く納得できてないので変更されます2014/06/02*/
