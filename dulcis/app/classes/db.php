8cb26e4a0ed7576f8f6f166a6c10e07457fb6c1a
iwnaga0202 <1101574@st.asojuku.ac.jp>
2014-06-03 10:30:18 +0900

    会員情報変更画面作成、データベースの接続先の変更、セッション終了のパーツを作成しました。　岩永

--- dulcis/app/classes/db.php
+++ dulcis/app/classes/db.php
@@ -1,10 +1,17 @@
 <?php
 //データベースへの接続情報
-//実装時には変更が必須
-define('db_host', 'localhost');
+//ローカル側
+/*define('db_host', 'localhost');
 define('db_user', 'root');
 define('db_pass', '0202');
 define('db_name', 'test');
+*/
+
+//サーバ側
+define('db_host', '172.20.17.214');
+define('db_user', 'user1');
+define('db_pass', '');
+define('db_name', 'dulcis');
 
 $dbc = mysqli_connect(db_host, db_user, db_pass, db_name);
 ?>
