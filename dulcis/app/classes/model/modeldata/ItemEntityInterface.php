bd4731817a46de69e36eb4f36bc3e802ebc6786d
Ryu Hanaoka <doradora56@gmail.com>
2014-06-05 11:15:21 +0900

    Itemインターフェース追加

---
+++ dulcis/app/classes/model/modeldata/ItemEntityInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Dulcis\Dulcis\model\modeldata;
+
+/**
+ * Interface CartEntityInterface
+ * 
+ * @package Dulcis\Dulcis\model\modeldata
+ *
+ * @author dora56
+ */
+interface ItemEntityInterface {
+    
+public function setId($id);
+public function getId();
+
+public function setName($name);
+public function getName();
+
+public function setGenre($genre);
+public function getGenre();
+
+public function setPriace($price);
+public function getPriace();
+
+public function setStock($price);
+public function getStock();
+
+public function setComment($comment);
+public function getComment();
+}
