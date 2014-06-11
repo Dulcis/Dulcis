c58afd011dde9102e902d6415be265eea20bf64c
Ryu Hanaoka <doradora56@gmail.com>
2014-06-11 13:05:27 +0900

    DAL

--- dulcis/app/classes/model/dal/DatabaseAdapterInterface.php
+++ dulcis/app/classes/model/dal/DatabaseAdapterInterface.php
@@ -17,18 +17,20 @@ namespace Dulcis\Dulcis\model\dal;
  */
 interface DatabaseAdapterInterface {
     
+    //DB接続と切断
     public function connect();
     public function disconnect();
-     
+    
+    //クエリ発行とプリペアドステートメント実行
     public function prepare($sql, array $options = array());
     public function execute(array $parameters = array());
-     
+    
+    //カラムのフェッチ
     public function fetch($fetchStyle = null,
         $cursorOrientation = null, $cursorOffset = null);
     public function fetchAll($fetchStyle = null, $column = 0);
      
-    public function select($table, array $bind,
-        $boolOperator = "AND");
+    public function select($table, array $bind, $boolOperator = "AND");
     public function insert($table, array $bind);
     public function update($table, array $bind, $where = "");
     public function delete($table, $where = "");
