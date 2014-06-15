6daec95ab617370da3600bdcfcc4772bb9927a98
dora56 <doradoora56@gmail.com>
2014-06-15 17:16:14 +0900

    ディレクトリ変更

---
+++ dulcis/app/classes/model/dal/factory/CreatePdoAdapter.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Dulcis\Dulcis\model\dal;
+
+require_once(dirname(__FILE__) . '/../../../../../../vendor/autoload.php');
+
+use Dulcis\Dulcis\model\dal\factory\LocalPdoAdapterFactry;
+use Exception;
+
+/**
+ * Description of CreatePdoAdapter
+ *
+ * @author dora56
+ */
+class CreatePdoAdapter {
+    
+    const LOCAL = "local";
+    const PUB = "pub";
+    
+    private $pdo;
+
+
+    public function __construct($status) {
+        $this->setPdoAdapter($status);
+    }
+    
+    public function setPdoAdapter($status) {
+        
+         switch ($status) {
+            case self::LOCAL:
+                $this->pdo = new LocalPdoAdapterFactry();               
+                break;
+            case self::PUB:
+                $this->pdo = null;
+            default:
+                throw new Exception("設定がまちがってます。");
+        }
+    }
+}
---
+++ dulcis/app/classes/model/dal/factory/PdoAdapterAbstractFactory.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Dulcis\Dulcis\model\dal\factory;
+
+/**
+ * Description of PdoAdapterFactory
+ *
+ * @author dora56
+ */
+abstract class PdoAdapterAbstractFactory {
+    
+    abstract public function create();
+}
