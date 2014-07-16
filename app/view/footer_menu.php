ba21d48b0f472d2c35ae2b5d1a631e2bd2d05228
Yudai Tani <nitta.yuu@gmail.com>
2014-07-16 12:40:23 +0900

    2014/07/16現在appの現状です。あとは、ロゴ、フッターカラー、購入処理の配置、くらいです

---
+++ app/view/footer_menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="../ref/css/footer.css" type="text/css" />
+<title>footer</title>
+</head>
+<body>
+<div id="footer">
+<a href="dulcis.html">会社概要</a>
+<a href="newcustomer.html">初めてのお客様へ</a>
+<a href="change.html">交換や返品について</a>
+<a href="info.html">採用情報</a>
+<a href="rules.php">利用規約</a>
+<a href="q_and_a.php">Q&A</a>
+</div>
+</body>
+</html>
